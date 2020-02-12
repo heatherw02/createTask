6e241b842670cf74a03de75678bc5f9e3ca86f3f
heatherw02 <60621346+heatherw02@users.noreply.github.com>
2020-02-12 11:47:37 -0800

    4 players

--- createTask.js
+++ createTask.js
@@ -6,11 +6,13 @@ Note: Everything should be working, but i want to add more players to the game a
 */
 
 let diceA, diceB, diceC
-let p1=10, p2=10 //nums of player's coins
+let p1=10, p2=10, p3=10, p4=10 //nums of player's coins
 let p1Bet1=0, p1Bet2=0, p1Bet3=0, p1Bet4=0, p1Bet5=0, p1Bet6=0 //player one bets
 let p2Bet1=0, p2Bet2=0, p2Bet3=0, p2Bet4=0, p2Bet5=0, p2Bet6=0
+let p3Bet1=0, p3Bet2=0, p3Bet3=0, p3Bet4=0, p3Bet5=0, p3Bet6=0
+let p4Bet1=0, p4Bet2=0, p4Bet3=0, p4Bet4=0, p4Bet5=0, p4Bet6=0
 let current_player = 1
-let test1 = 0, test2 = 0
+let test1 = 0, test2 = 0, test3 = 0, test4 = 0
 
 function roll()
 {
@@ -157,10 +159,18 @@ function roll()
 
             test2 = (p2Bet1 * deer) + (p2Bet2 * fruit) + (p2Bet3 * chick) + (p2Bet4 * fish) +  (p2Bet5 * crab) + (p2Bet6 * lobster)
             p2 += test2
+
+            test3 = (p3Bet1 * deer) + (p3Bet2 * fruit) + (p3Bet3 * chick) + (p3Bet4 * fish) +  (p3Bet5 * crab) + (p3Bet6 * lobster)
+            p3 += test3
+
+            test4 = (p4Bet1 * deer) + (p4Bet2 * fruit) + (p4Bet3 * chick) + (p4Bet4 * fish) +  (p4Bet5 * crab) + (p4Bet6 * lobster)
+            p4 += test4
             
     //setting bets back to zero  
     p1Bet1=0, p1Bet2=0, p1Bet3=0, p1Bet4=0, p1Bet5=0, p1Bet6=0 
     p2Bet1=0, p2Bet2=0, p2Bet3=0, p2Bet4=0, p2Bet5=0, p2Bet6=0
+    p3Bet1=0, p3Bet2=0, p3Bet3=0, p3Bet4=0, p3Bet5=0, p3Bet6=0
+    p4Bet1=0, p4Bet2=0, p4Bet3=0, p4Bet4=0, p4Bet5=0, p4Bet6=0
            
 
     document.getElementById('p1-coins').innerHTML = 'Coins: ' + p1
@@ -178,20 +188,44 @@ function roll()
 
 function hold()
 {
-    if(current_player===2)
+    if(current_player===1)
     {     
-        document.getElementById('p1-background').style.backgroundColor = 'pink'  
-        document.getElementById('p2-background').style.backgroundColor = 'grey'
-   
-        current_player=1
-    }
-    else
-    { 
         document.getElementById('p1-background').style.backgroundColor = 'grey'    
         document.getElementById('p2-background').style.backgroundColor = 'pink'
+        document.getElementById('p3-background').style.backgroundColor = 'grey'
+        document.getElementById('p4-background').style.backgroundColor = 'grey'
 
         current_player=2
     }
+    else if(current_player===2)
+    { 
+        document.getElementById('p1-background').style.backgroundColor = 'grey'    
+        document.getElementById('p2-background').style.backgroundColor = 'grey'
+        document.getElementById('p3-background').style.backgroundColor = 'pink'
+        document.getElementById('p4-background').style.backgroundColor = 'grey'
+
+        current_player=3
+    }
+    else if(current_player===3)
+    {
+        document.getElementById('p1-background').style.backgroundColor = 'grey'    
+        document.getElementById('p2-background').style.backgroundColor = 'grey'
+        document.getElementById('p3-background').style.backgroundColor = 'grey'
+        document.getElementById('p4-background').style.backgroundColor = 'pink'
+
+        current_player=4
+    }
+    /*
+    else(current_player===4)
+    {
+        document.getElementById('p1-background').style.backgroundColor = 'pink'    
+        document.getElementById('p2-background').style.backgroundColor = 'grey'
+        document.getElementById('p3-background').style.backgroundColor = 'grey'
+        document.getElementById('p4-background').style.backgroundColor = 'grey'
+
+        current_player=1
+    }
+    */
 }
 
 
@@ -227,7 +261,7 @@ function click1()
         }
         
     }
-    else
+    else if(current_player===2)
     {
         if(p2===0)
         { bye1(current_player)
@@ -239,6 +273,38 @@ function click1()
         document.getElementById('p2-coins').innerHTML = 'Coins: ' + p2
         document.getElementById('p2betA').innerHTML = 'Bets: Deer = ' + p2Bet1 + ', Fruit = ' + p2Bet2
             if(p2===0)
+            {    bye2(current_player)
+            }
+        }
+    }
+    else if(current_player===3)
+    {
+        if(p3===0)
+        { bye1(current_player)
+        }
+        else
+        {
+        p3Bet1++
+        p3--
+        document.getElementById('p3-coins').innerHTML = 'Coins: ' + p3
+        document.getElementById('p3betA').innerHTML = 'Bets: Deer = ' + p3Bet1 + ', Fruit = ' + p3Bet2
+            if(p3===0)
+            {    bye2(current_player)
+            }
+        }
+    }
+    else(current_player===4)
+    {
+        if(p4===0)
+        { bye1(current_player)
+        }
+        else
+        {
+        p4Bet1++
+        p4--
+        document.getElementById('p4-coins').innerHTML = 'Coins: ' + p4
+        document.getElementById('p4betA').innerHTML = 'Bets: Deer = ' + p4Bet1 + ', Fruit = ' + p4Bet2
+            if(p4===0)
             {    bye2(current_player)
             }
         }
@@ -263,7 +329,7 @@ function click2()
             }
         }
     }
-    else
+    else if(current_player===2)
     {
         if(p2===0)
         { bye1(current_player)
@@ -275,6 +341,38 @@ function click2()
         document.getElementById('p2-coins').innerHTML = 'Coins: ' + p2
         document.getElementById('p2betA').innerHTML = 'Bets: Deer = ' + p2Bet1 + ', Fruit = ' + p2Bet2
             if(p2===0)
+            {   bye2(current_player)
+            }
+        }
+    }
+    else if(current_player===3)
+    {
+        if(p3===0)
+        { bye1(current_player)
+        }
+        else
+        {
+        p3Bet2++
+        p3--
+        document.getElementById('p3-coins').innerHTML = 'Coins: ' + p3
+        document.getElementById('p3betA').innerHTML = 'Bets: Deer = ' + p3Bet1 + ', Fruit = ' + p3Bet2
+            if(p3===0)
+            {   bye2(current_player)
+            }
+        }
+    }
+    else(current_player===4)
+    {
+        if(p4===0)
+        { bye1(current_player)
+        }
+        else
+        {
+        p4Bet2++
+        p4--
+        document.getElementById('p4-coins').innerHTML = 'Coins: ' + p4
+        document.getElementById('p4betA').innerHTML = 'Bets: Deer = ' + p4Bet1 + ', Fruit = ' + p4Bet2
+            if(p4===0)
             {   bye2(current_player)
             }
         }
@@ -299,7 +397,7 @@ function click3()
             }
         }
     }
-    else
+    else if(current_player===2)
     {
         if(p2===0)
         { bye1(current_player)
@@ -311,6 +409,38 @@ function click3()
         document.getElementById('p2-coins').innerHTML = 'Coins: ' + p2
         document.getElementById('p2betB').innerHTML = 'Rooster = ' + p2Bet3 + ', Fish = ' + p2Bet4
             if(p2===0)
+            {   bye2(current_player)
+            }
+        }
+    }
+    else if(current_player===3)
+    {
+        if(p3===0)
+        { bye1(current_player)
+        }
+        else
+        {
+        p3Bet3++
+        p3--
+        document.getElementById('p3-coins').innerHTML = 'Coins: ' + p3
+        document.getElementById('p3betB').innerHTML = 'Rooster = ' + p3Bet3 + ', Fish = ' + p3Bet4
+            if(p2===0)
+            {   bye2(current_player)
+            }
+        }
+    }
+    else(current_player===4)
+    {
+        if(p4===0)
+        { bye1(current_player)
+        }
+        else
+        {
+        p4Bet3++
+        p4--
+        document.getElementById('p4-coins').innerHTML = 'Coins: ' + p4
+        document.getElementById('p4betB').innerHTML = 'Rooster = ' + p4Bet3 + ', Fish = ' + p4Bet4
+            if(p4===0)
             {   bye2(current_player)
             }
         }
@@ -335,7 +465,7 @@ function click4()
             }
         }
     }
-    else
+    else if(current_player===2)
     {
         if(p2===0)
         { bye1(current_player)
@@ -347,6 +477,38 @@ function click4()
         document.getElementById('p2-coins').innerHTML = 'Coins: ' + p2
         document.getElementById('p2betB').innerHTML = 'Rooster = ' + p2Bet3 + ', Fish = ' + p2Bet4
             if(p2===0)
+            {   bye2(current_player)
+            }
+        }
+    }
+    else if(current_player===3)
+    {
+        if(p3===0)
+        { bye1(current_player)
+        }
+        else
+        {
+        p3Bet4++
+        p3--
+        document.getElementById('p3-coins').innerHTML = 'Coins: ' + p3
+        document.getElementById('p3betB').innerHTML = 'Rooster = ' + p3Bet3 + ', Fish = ' + p3Bet4
+            if(p3===0)
+            {   bye2(current_player)
+            }
+        }
+    }
+    else(current_player===4)
+    {
+        if(p4===0)
+        { bye1(current_player)
+        }
+        else
+        {
+        p4Bet4++
+        p4--
+        document.getElementById('p4-coins').innerHTML = 'Coins: ' + p4
+        document.getElementById('p4betB').innerHTML = 'Rooster = ' + p4Bet3 + ', Fish = ' + p4Bet4
+            if(p4===0)
             {   bye2(current_player)
             }
         }
@@ -371,7 +533,7 @@ function click5()
             }
         }
     }
-    else
+    else if(current_player===2)
     {
         if(p2===0)
         {bye1(current_player)
@@ -383,6 +545,38 @@ function click5()
          document.getElementById('p2-coins').innerHTML = 'Coins: ' + p2
          document.getElementById('p2betC').innerHTML = 'Crab = ' + p2Bet5 + ', Lobster = ' + p2Bet6
             if(p2===0)
+            {   bye2(current_player)
+            }
+        }
+    }
+    else if(current_player===3)
+    {
+        if(p3===0)
+        {bye1(current_player)
+        }
+        else
+        {
+         p3Bet5++
+         p3--
+         document.getElementById('p3-coins').innerHTML = 'Coins: ' + p3
+         document.getElementById('p3betC').innerHTML = 'Crab = ' + p3Bet5 + ', Lobster = ' + p3Bet6
+            if(p3===0)
+            {   bye2(current_player)
+            }
+        }
+    }
+    else(current_player===4)
+    {
+        if(p4===0)
+        {bye1(current_player)
+        }
+        else
+        {
+         p4Bet5++
+         p4--
+         document.getElementById('p4-coins').innerHTML = 'Coins: ' + p4
+         document.getElementById('p4betC').innerHTML = 'Crab = ' + p4Bet5 + ', Lobster = ' + p4Bet6
+            if(p4===0)
             {   bye2(current_player)
             }
         }
@@ -407,7 +601,7 @@ function click6()
             }
         }
     }
-    else
+    else if(current_player===2)
     {
         if(p2===0)
         { bye1(current_player)
@@ -419,6 +613,38 @@ function click6()
         document.getElementById('p2-coins').innerHTML = 'Coins: ' + p2
         document.getElementById('p2betC').innerHTML = 'Crab = ' + p2Bet5 + ', Lobster = ' + p2Bet6
             if(p2===0)
+            {   bye2(current_player)
+            }
+        }
+    }
+    else if(current_player===2)
+    {
+        if(p3===0)
+        { bye1(current_player)
+        }
+        else
+        {
+        p3Bet6++
+        p3--
+        document.getElementById('p3-coins').innerHTML = 'Coins: ' + p3
+        document.getElementById('p3betC').innerHTML = 'Crab = ' + p3Bet5 + ', Lobster = ' + p3Bet6
+            if(p3===0)
+            {   bye2(current_player)
+            }
+        }
+    }
+    else(current_player===4)
+    {
+        if(p4===0)
+        { bye1(current_player)
+        }
+        else
+        {
+        p4Bet6++
+        p4--
+        document.getElementById('p4-coins').innerHTML = 'Coins: ' + p4
+        document.getElementById('p4betC').innerHTML = 'Crab = ' + p4Bet5 + ', Lobster = ' + p4Bet6
+            if(p4===0)
             {   bye2(current_player)
             }
         }
